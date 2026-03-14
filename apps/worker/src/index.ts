162faf0908077ef0d4e94bd566431d6d5426fc56
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-03-14 13:20:38 +0530

    Added script for packages

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,9 +1,16 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get('/health', (c) => {
+  return c.text('Working!');
+});
+
+app.post('/', (c) => {
+  const payload = c.req.json();
+
+  console.log(payload);
+  return c.text('got the request');
+});
 
 export default app
