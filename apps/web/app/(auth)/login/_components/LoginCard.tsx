6c60eca314b8b5c5699b7d4fa6c080fe5e33abd9
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-04-10 15:21:46 +0530

    Added: login and sign up forms

--- apps/web/app/(auth)/login/_components/LoginCard.tsx
+++ apps/web/app/(auth)/login/_components/LoginCard.tsx
@@ -8,12 +8,76 @@ import {
     CardFooter,
     CardHeader,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { useLogin } from "@/hooks/use-auth";
+import { validateLoginForm, validateSignupForm } from "@rum-core/shared";
 import Link from "next/link";
+import { useCallback, useEffect, useState } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
+import { toast } from "sonner";
 
 export default function LoginCard() {
     const { loginWithGithub, loginWithGoogle } = useLogin();
+    const [mode, setMode] = useState("login");
+
+    // Form states
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errors, setErrors] = useState<Record<string, string> | null>(null);
+
+    function toggleLoginForm() {
+        setMode(mode === "login" ? "signup" : "login");
+        setErrors(null);
+    }
+
+    const handleSubmit = useCallback(() => {
+        if (mode === "login") {
+            const result = validateLoginForm({ email, password });
+            if (!result.isValid) {
+                setErrors(result.errors);
+                return;
+            }
+            setErrors(null);
+            console.log("Login:", { email, password });
+        } else {
+            const result = validateSignupForm({
+                name,
+                email,
+                password,
+                confirmPassword,
+            });
+            if (!result.isValid) {
+                setErrors(result.errors);
+                return;
+            }
+            setErrors(null);
+            console.log("Sign Up:", { name, email, password });
+        }
+    }, [confirmPassword, email, mode, name, password]);
+
+    useEffect(() => {
+        if (!errors) return;
+        toast.error("Error ", {
+            description: () => {
+                return Object.entries(errors).map(([key, value]) => (
+                    <p key={key}>{value}</p>
+                ));
+            },
+        });
+    }, [errors]);
+
+    useEffect(() => {
+        function handleEnter(event: KeyboardEvent) {
+            if (event.key !== "Enter") return;
+            handleSubmit();
+        }
+        window.addEventListener("keydown", handleEnter);
+        return () => {
+            window.removeEventListener("keydown", handleEnter);
+        };
+    }, [handleSubmit]);
 
     return (
         <Card className="w-full max-w-sm py-7 bg-foreground/5 rounded-lg backdrop-blur-lg gap-8">
@@ -25,15 +89,111 @@ export default function LoginCard() {
                     <h4 className="text-foreground/20 text-2xl">rum core</h4>
                 </div>
             </CardHeader>
-            <CardContent className="flex flex-col gap-8 px-7">
-                <div className="text-center">
-                    <h5 className="text-lg">Welcom back</h5>
+            <CardContent className="flex flex-col px-7">
+                {/* welcome */}
+                <div className="text-center mb-10">
+                    <h5 className="text-lg">
+                        {mode === "login" ? "Welcome back" : "Sign Up"}
+                    </h5>
                     <p className="text-xs text-foreground/60">
-                        Sign in to your account to continue
+                        {mode === "login"
+                            ? "Sign in to your account to continue"
+                            : "Create a new account to get started"}
                     </p>
                 </div>
 
-                <div className="flex flex-col gap-3">
+                {/* form */}
+                <div className="flex flex-col gap-3 mb-3">
+                    {mode === "login" ? (
+                        <>
+                            <Input
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 text-lg"
+                                placeholder="Email"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <Input
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 text-lg"
+                                placeholder="Password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Button
+                                variant="outline"
+                                onClick={handleSubmit}
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 group"
+                            >
+                                <span>Login</span>
+                            </Button>
+                        </>
+                    ) : (
+                        <>
+                            <Input
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 text-lg"
+                                placeholder="Name"
+                                type="text"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                            <Input
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 text-lg"
+                                placeholder="Email"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                            <Input
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 text-lg"
+                                placeholder="Password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            <Input
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 text-lg"
+                                placeholder="Confirm Password"
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) =>
+                                    setConfirmPassword(e.target.value)
+                                }
+                            />
+                            <Button
+                                variant="outline"
+                                onClick={handleSubmit}
+                                className="w-full rounded-md dark:bg-foreground/5 dark:hover:bg-foreground/10 group"
+                            >
+                                <span>Sign Up</span>
+                            </Button>
+                        </>
+                    )}
+                    <div className="flex justify-between items-center gap-1 text-foreground/60 text-xs mt-1">
+                        <Button
+                            variant={"ghost"}
+                            className="hover:underline h-fit w-fit px-1 rounded"
+                            onClick={toggleLoginForm}
+                            type="button"
+                        >
+                            {mode === "login" ? "Sign Up" : "Login"}
+                        </Button>
+                        {mode === "login" && (
+                            <Button
+                                variant={"ghost"}
+                                className="hover:underline h-fit w-fit px-1 rounded"
+                            >
+                                Forgot Password
+                            </Button>
+                        )}
+                    </div>
+                </div>
+
+                {/* separator */}
+                <div className="w-full border-t mb-8" />
+
+                {/* oauth */}
+                <div className="flex flex-col gap-3 mb-6">
                     <Button
                         variant="outline"
                         onClick={loginWithGoogle}
@@ -52,6 +212,7 @@ export default function LoginCard() {
                     </Button>
                 </div>
 
+                {/* quick links */}
                 <div className="flex justify-center items-center gap-1 text-foreground/60 text-xs">
                     <Link href="/privacy" className="hover:underline">
                         privacy policy
