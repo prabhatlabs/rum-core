3a89a7991207c5bbbb34be053775b73f8672f60e
Prabhat <workforprabhat1254@gmail.com>
2026-06-29 10:58:27 +0530

    responsiveness

--- apps/web/app/_components/Navbar.tsx
+++ apps/web/app/_components/Navbar.tsx
@@ -3,8 +3,72 @@
 import { Logo } from "@/components/Logo";
 import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { MenuIcon } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
+
+const links = [
+    { name: "Home", href: "#home", target: "_self" },
+    { name: "Features", href: "#features", target: "_self" },
+    { name: "How to use?", href: "#how-to-use", target: "_self" },
+    { name: "Pricing", href: "#pricing", target: "_self" },
+    { name: "Contact", href: "https://prabhatlabs.dev", target: "_blank" },
+];
+
+function LgNavItems() {
+    return (
+        <div className="hidden lg:flex items-center gap-4">
+            {links.map((link) => (
+                <Link
+                    key={link.name}
+                    className="px-2 py-1"
+                    href={link.href}
+                    target={link.target}
+                >
+                    {link.name}
+                </Link>
+            ))}
+        </div>
+    );
+}
+
+function NavItems() {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+        <>
+            <Button
+                variant={"outline"}
+                size="icon"
+                onClick={() => setIsOpen((p) => !p)}
+            >
+                <MenuIcon />
+            </Button>
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div
+                        initial={{ opacity: 0, y: -10 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -10 }}
+                        transition={{ duration: 0.2 }}
+                        className="absolute top-full left-0 w-full flex flex-col justify-center items-center gap-4 py-4 bg-background/90 border-b"
+                    >
+                        {links.map((link) => (
+                            <Link
+                                key={link.name}
+                                className="px-2 py-1"
+                                href={link.href}
+                                target={link.target}
+                            >
+                                {link.name}
+                            </Link>
+                        ))}
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </>
+    );
+}
 
 export default function Navbar() {
     return (
@@ -26,26 +90,8 @@ export default function Navbar() {
                 </div>
 
                 <div className="flex items-center justify-center gap-4 z-10 text-xs md:text-sm">
-                    <Link className="px-2 py-1" href={"#home"}>
-                        Home
-                    </Link>
-                    <Link className="px-2 py-1" href={"#features"}>
-                        Features
-                    </Link>
-                    <Link className="px-2 py-1" href={"#how-to-use"}>
-                        How to use?
-                    </Link>
-                    <Link className="px-2 py-1" href={"#pricing"}>
-                        Pricing
-                    </Link>
-                    <Link
-                        className="px-2 py-1"
-                        href={"https://prabhatlabs.dev"}
-                        target="_blank"
-                    >
-                        Contact
-                    </Link>
-
+                    <LgNavItems />
+                    <NavItems />
                     <Link href={"/dashboard"}>
                         <Button variant={"outline"}>Login</Button>
                     </Link>
